a663dc850946d349078ad41c6fd83dcb610f5eef
Eric Kinzie <eric@qosient.com>
2017-01-19 13:49:13 -0500

    Argus timer library
    This adds a library of routines for maintaining a large number of
    timers.  This is designed to be usable from the ArgusClientTimeout()
    function.  A "hashed wheel" with a configurable number of slots
    (teeth?) holds references to the timers.  Each slot has a sorted
    tree of pointers to timer structures, ordered by expiration time.
    
    When a timer is inserted into the wheel, a slot is chosen by finding
    the expiration time modulo the number of slots.  The timer structure
    is added to the balanced tree in that slot.
    
    Every time the wheel is advanced (once per ArgusClientTimeout() for
    example) the timer tree in the current slot is searched for timers
    that expire sometime during the current period.  Each timer specifies
    a callback function which is invoked at this time and the timer
    structure is removed from the tree.
    
    Once every revolution of the timer wheel, the wheel's time is synced
    with the operating systems's time.  If the wheel got little behind,
    fire all of the timers that would have been missed because of jumping
    to the new time.  This happens some on systems that don't support
    clock_nanosleep() (with absolute times).
    
    A timer is stopped by simply removing it from its tree.
    
    For systems that support clock_nanosleep(), use the monotonic clock
    and absolute times to keep argus_timer's internal "clock"
    (argus_timer_wheel->now) from drifting too far.

---
+++ common/argus_timer.c
@@ -0,0 +1,316 @@
+/*
+ * Gargoyle Client Software. Tools to read, analyze and manage Argus data.
+ * Copyright (c) 2017 QoSient, LLC
+ * All rights reserved.
+ *
+ * THE ACCOMPANYING PROGRAM IS PROPRIETARY SOFTWARE OF QoSIENT, LLC,
+ * AND CANNOT BE USED, DISTRIBUTED, COPIED OR MODIFIED WITHOUT
+ * EXPRESS PERMISSION OF QoSIENT, LLC.
+ *
+ * QOSIENT, LLC DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL QOSIENT, LLC BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ */
+
+/*
+ * Gargoyle timer library
+ * Eric Kinzie <eric@qosient.com>
+ *
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+#include "argus_config.h"
+#include "argus_timer.h"
+#include "argus_util.h"
+
+unsigned missed = 0;
+unsigned replayed = 0;
+unsigned recovered = 0;
+
+#define TS_MSEC(ts) ((ts)->tv_sec*1000+(ts)->tv_nsec/1000000)
+static unsigned
+__slot(struct argus_timer_wheel *w, struct timespec *when)
+{
+   return (unsigned)((TS_MSEC(when)/TS_MSEC(&w->period)) % w->nslots);
+}
+
+static inline void
+__advance(struct argus_timer_wheel *w)
+{
+   struct timespec sum;
+
+   __timespec_add(&w->now, &w->period, &sum);
+   w->now = sum;
+   w->current = (w->current + 1) % w->nslots;
+}
+
+static int
+__gettime_default(struct timespec *ts)
+{
+#if HAVE_CLOCK_GETTIME
+   return clock_gettime(CLOCK_MONOTONIC, ts);
+#else
+   struct timeval tv;
+   int rv;
+
+   rv = gettimeofday(&tv, NULL);
+   ts->tv_sec = tv.tv_sec;
+   ts->tv_nsec = tv.tv_usec * 1000;
+   return rv;
+#endif
+}
+
+int
+ArgusTimerSleep(struct argus_timer_wheel *w)
+{
+   int rv;
+   struct timespec req;
+
+#if HAVE_CLOCK_NANOSLEEP
+   __timespec_add(&w->now, &w->period, &req);
+   while ((rv = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &req, NULL)) == EINTR);
+#else
+   struct timespec rem;
+
+   req = w->period;
+   while ((rv = nanosleep(&req, &rem)) == EINTR)
+      req = rem;
+#endif
+
+   return rv;
+}
+
+struct argus_timer_wheel *
+ArgusTimerWheel(unsigned nslots, const struct timespec * const period,
+                gettimefunc gettime)
+{
+   struct argus_timer_wheel *tmp;
+   unsigned u;
+   size_t slotmem;
+
+   tmp = malloc(sizeof(*tmp));
+   if (tmp == NULL)
+      goto out;
+
+   memset(tmp, 0, sizeof(*tmp));
+   tmp->period = *period;
+
+   if (gettime)
+      tmp->gettime = gettime;
+   else
+      tmp->gettime = __gettime_default;
+
+   tmp->nslots = nslots;
+   slotmem = sizeof(*(tmp->slots)) * nslots;
+   tmp->slots = malloc(slotmem);
+   if (tmp->slots == NULL) {
+      free(tmp);
+      tmp = NULL;
+      goto out;
+   }
+
+   memset(tmp->slots, 0, slotmem);
+   for (u = 0; u < nslots; u++) {
+      tmp->slots[u] = malloc(sizeof(*(tmp->slots[0])));
+      RB_INIT(tmp->slots[u]);
+   }
+
+out:
+   return tmp;
+}
+
+int
+ArgusTimerFreeWheel(struct argus_timer_wheel *w)
+{
+   int rv = 0;
+   unsigned u;
+
+   if (w == NULL)
+      return -1;
+
+   if (w->slots == NULL) {
+      rv = -1;
+      goto free_wheel;
+   }
+
+   /* don't free the timer trees since each of those structres may be
+    * referenced outside of this library.  That is the responsibility of
+    * the caller.
+    */
+
+   for (u = 0; u < w->nslots; u++) {
+      struct argus_timer_tree *tree = w->slots[u];
+      while (!RB_EMPTY(tree))
+         RB_REMOVE(argus_timer_tree, tree, RB_MIN(argus_timer_tree, tree));
+      free(tree);
+   }
+   free(w->slots);
+
+free_wheel:
+   free(w);
+
+   return rv;
+}
+
+static struct argus_timer *
+__argus_timer_start(struct argus_timer_wheel *w, struct timespec *exp,
+                    callback_t callback, cleanup_t cleanup,
+                    void *callbackdata, int relative)
+{
+   struct argus_timer *tim;
+   unsigned slot;
+
+   /* If we're just starting the first timer, make sure we have the current
+    * time and are referencing the correct slot in the wheel.
+    */
+   if (w->ntimers == 0) {
+       struct timespec currenttime;
+
+       w->gettime(&currenttime);
+       w->now = currenttime;
+       w->current = __slot(w, &currenttime);
+   }
+
+   tim = malloc(sizeof(*tim));
+   memset(&tim->tree, 0, sizeof(tim->tree));
+
+   if (relative) {
+      struct timespec expabs;
+
+      __timespec_add(&w->now, exp, &expabs);
+      tim->expiry = expabs;
+   } else {
+      tim->expiry = *exp;
+   }
+
+   tim->callback = callback;
+   tim->cleanup = cleanup;
+   tim->data = callbackdata;
+   tim->td = w->distinguisher++;
+   w->ntimers++;
+
+   slot = __slot(w, &tim->expiry);
+   RB_INSERT(argus_timer_tree, w->slots[slot], tim);
+
+   return tim;
+}
+
+struct argus_timer *
+ArgusTimerStartRelative(struct argus_timer_wheel *w, struct timespec *exp,
+                        callback_t callback, cleanup_t cleanup,
+                        void *callbackdata)
+{
+   return __argus_timer_start(w, exp, callback, cleanup, callbackdata, 1);
+}
+
+struct argus_timer *
+ArgusTimerStartAbsolute(struct argus_timer_wheel *w, struct timespec *exp,
+                        callback_t callback, cleanup_t cleanup,
+                        void *callbackdata)
+{
+   return __argus_timer_start(w, exp, callback, cleanup, callbackdata, 0);
+}
+
+void
+ArgusTimerStop(struct argus_timer_wheel *w, struct argus_timer *tim)
+{
+   unsigned slot = __slot(w, &tim->expiry);
+   RB_REMOVE(argus_timer_tree, w->slots[slot], tim);
+}
+
+static unsigned
+__fire_timers(struct argus_timer_wheel *w,
+              struct argus_timer_tree *tree,
+              const struct timespec * const currenttime)
+{
+   struct argus_timer *tim;
+   unsigned count = 0;
+   int done = 0;
+
+   if (RB_EMPTY(tree))
+      done = 1;
+
+   while (!done) {
+      tim = RB_MIN(argus_timer_tree, tree);
+      if (TS_MSEC(&tim->expiry) <= TS_MSEC(currenttime)) {
+         ArgusTimerResult res;
+
+         count++;
+         RB_REMOVE(argus_timer_tree, tree, tim);
+         w->ntimers--;
+         if ((res = tim->callback(tim, &w->now)) < FINISHED) {
+            /* reschedule - callback updated expiry */
+            __argus_timer_start(w, &tim->expiry, tim->callback, tim->cleanup,
+                                tim->data, (int)res);
+         } else {
+            if (tim->cleanup)
+               tim->cleanup(tim->data);
+         }
+      } else {
+         done = 1;
+      }
+      if (RB_EMPTY(tree))
+         done = 1;
+   }
+   return count;
+}
+
+int
+ArgusTimerAdvanceWheel(struct argus_timer_wheel *w)
+{
+   /* see if anything in our current slot has expired.  If so, remove
+    * the timer from the wheel, run the callback, reschedule if
+    * necessary.  Then advance the wheel.
+    */
+
+   struct argus_timer_tree *tree = w->slots[w->current];
+   struct argus_timer *tim;
+   int done = 0;
+
+   /* Look for the timers in the tree that expire soonest and see if
+    * that's now.
+    */
+
+   __fire_timers(w, tree, &w->now);
+
+   if (w->current) {
+      __advance(w);
+   } else {
+      /* once every revolution check for procession (or the system
+       * clock being set).  This will be minimized by using absolute
+       * timeouts in __nanosleep().
+       */
+       struct timespec currenttime;
+       struct timespec diff;
+       unsigned msec_per_slot;
+
+       w->gettime(&currenttime);
+       __timespec_sub(&w->now, &currenttime, &diff);
+       /* this needs to take into account the update period */
+       if (diff.tv_sec < 0) {
+          /* if our time is behind, advance the wheel until we're
+           * caught up.
+           */
+           unsigned target_slot = __slot(w, &currenttime);
+           while (w->current != target_slot) {
+              recovered += __fire_timers(w, w->slots[w->current], &currenttime);
+              __advance(w);
+           }
+           missed++;
+       } else /* if some threshold such as diff.tv_sec > 0 */ {
+           /* unlikely */
+       }
+
+       w->now = currenttime;
+       w->current = __slot(w, &currenttime);
+   }
+
+   return 0;
+}
---
+++ include/argus_timer.h
@@ -0,0 +1,162 @@
+/*
+ * Gargoyle Client Software. Tools to read, analyze and manage Argus data.
+ * Copyright (c) 2017 QoSient, LLC
+ * All rights reserved.
+ *
+ * THE ACCOMPANYING PROGRAM IS PROPRIETARY SOFTWARE OF QoSIENT, LLC,
+ * AND CANNOT BE USED, DISTRIBUTED, COPIED OR MODIFIED WITHOUT
+ * EXPRESS PERMISSION OF QoSIENT, LLC.
+ *
+ * QOSIENT, LLC DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS, IN NO EVENT SHALL QOSIENT, LLC BE LIABLE FOR ANY
+ * SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ *
+ */
+
+/*
+ * Gargoyle timer library
+ * Eric Kinzie <eric@qosient.com>
+ *
+ */
+
+#ifndef __ARGUS_TIMER_H
+# define __ARGUS_TIMER_H
+
+# include "argus_config.h"
+
+# include <time.h>
+# include <sys/types.h>
+# if HAVE_STDINT_H
+#  include <stdint.h>
+# endif
+
+# include "bsd/sys/tree.h"
+
+typedef enum __argus_timer_result {
+   RESCHEDULE_ABS = 0,
+   RESCHEDULE_REL = 1,
+   FINISHED,
+} ArgusTimerResult;
+
+struct argus_timer;
+typedef ArgusTimerResult (*callback_t)(struct argus_timer *, struct timespec *);
+typedef void (*cleanup_t)(void *); /* can be free() */
+typedef int (*gettimefunc)(struct timespec *);
+
+RB_HEAD(argus_timer_tree, argus_timer);
+
+/* The "td" field is used to differentiate timers with the same
+ * expiration time.  Timer trees are ordered by time and the RB tree
+ * implementation will not insert duplicates.
+ */
+struct argus_timer {
+   struct timespec expiry;
+   uint64_t td; /* timer distinguisher */
+   callback_t callback;
+   cleanup_t cleanup;
+   void *data;
+   RB_ENTRY(argus_timer) tree;
+};
+
+struct argus_timer_wheel {
+   /* circular array of timer trees */
+   struct argus_timer_tree **slots;
+   unsigned nslots;
+   unsigned current;	/* 0 <= current < nslots */
+   struct timespec period; /* update period - only good down to milliseconds */
+   struct timespec now;	/* incremented once each update period */
+   uint64_t ntimers;	/* number of timers on the wheel */
+   uint64_t distinguisher;
+   gettimefunc gettime;
+};
+
+struct argus_timer_wheel *
+ArgusTimerWheel(unsigned, const struct timespec * const, gettimefunc);
+
+struct argus_timer *
+ArgusTimerStartRelative(struct argus_timer_wheel *, struct timespec *,
+                        callback_t, cleanup_t, void *);
+
+struct argus_timer *
+ArgusTimerStartAbsolute(struct argus_timer_wheel *, struct timespec *,
+                        callback_t, cleanup_t, void *);
+
+void
+ArgusTimerStop(struct argus_timer_wheel *, struct argus_timer *);
+
+struct argus_timer_wheel *
+ArgusTimerAllocateWheel(unsigned, struct timespec *, gettimefunc);
+
+int
+ArgusTimerFreeWheel(struct argus_timer_wheel *);
+
+int
+ArgusTimerSleep(struct argus_timer_wheel *);
+
+/* ArgusTimerAdvanceWheel can be buried inside of a client's
+ * ArgusClientTimeout() function so that it is called once per second,
+ * assuming that's the desired period.
+ */
+int
+ArgusTimerAdvanceWheel(struct argus_timer_wheel *);
+
+static int
+__argus_timer_compare(struct argus_timer *a, struct argus_timer *b)
+{
+   struct timespec result;
+
+   result.tv_sec = a->expiry.tv_sec - b->expiry.tv_sec;
+   result.tv_nsec = a->expiry.tv_nsec - b->expiry.tv_nsec;
+   if (result.tv_nsec < 0) {
+      --result.tv_sec;
+      result.tv_nsec += 1000000000;
+   }
+   if (result.tv_sec < 0)
+      return -1;
+   if (result.tv_sec > 0)
+      return 1;
+
+   /* times are the same.  compare distinguishers */
+   if (a->td < b->td)
+      return -1;
+   if (a->td > b->td)
+      return 1;
+   return 0;
+}
+
+RB_GENERATE_STATIC(argus_timer_tree, argus_timer, tree, __argus_timer_compare);
+
+
+static inline void
+__timespec_sub(const struct timespec * const a,
+               const struct timespec * const b,
+               struct timespec *result)
+{
+    result->tv_sec = a->tv_sec - b->tv_sec;
+    result->tv_nsec = a->tv_nsec - b->tv_nsec;
+    if (result->tv_nsec < 0) {
+      --result->tv_sec;
+      result->tv_nsec += 1000000000;
+    }
+}
+
+static inline void
+__timespec_add(const struct timespec * const a,
+               const struct timespec * const b,
+               struct timespec *result)
+{
+    result->tv_sec = a->tv_sec + b->tv_sec;
+    result->tv_nsec = a->tv_nsec + b->tv_nsec;
+    if (result->tv_nsec >= 1000000000)
+      {
+        ++result->tv_sec;
+        result->tv_nsec -= 1000000000;
+      }
+}
+
+#endif
