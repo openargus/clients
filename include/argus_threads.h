bcb4076c16381090bc73e81bf4b75a6496de95da
Eric Kinzie <eric@qosient.com>
2017-03-10 16:49:39 -0500

    argus_threads: add MUTEX_DESTROY macro
    An initialization macro was already present.

--- include/argus_threads.h
+++ include/argus_threads.h
@@ -6,12 +6,14 @@
 #    define MUTEX_LOCK(l) pthread_mutex_lock(l)
 #    define MUTEX_UNLOCK(l) pthread_mutex_unlock(l)
 #    define MUTEX_INIT(l, a) pthread_mutex_init((l), (a))
+#    define MUTEX_DESTROY(l) pthread_mutex_destroy(l);
 #    define COND_SIGNAL(cond) pthread_cond_signal(cond)
 
 #  else /* ARGUS_THREADS */
 #    define MUTEX_LOCK(l) (0)
 #    define MUTEX_UNLOCK(l) (0)
 #    define MUTEX_INIT(l, a) (0)
+#    define MUTEX_DESTROY(l) (0)
 #    define COND_SIGNAL(cond) (0)
 
 #  endif /* ARGUS_THREADS */
