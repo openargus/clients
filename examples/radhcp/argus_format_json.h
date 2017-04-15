07892daa3c6f61f85b40a8ba1cc795476ce47c3b
Eric Kinzie <eric@qosient.com>
2017-04-14 21:40:24 -0400

    radhcp: add textual output formatting functions
    The first format added is JSON.  argus_format_json.[ch]
    
    Use ArgusJsonObjOnlyFormatterTable to generate output for QoSient
    CGI scripts.

---
+++ examples/radhcp/argus_format_json.h
@@ -0,0 +1,8 @@
+#ifndef __ARGUS_FORMAT_JSON_H
+# define __ARGUS_FORMAT_JSON_H
+# include "argus_print.h"
+
+const struct ArgusFormatterTable ArgusJsonFormatterTable;
+const struct ArgusFormatterTable ArgusJsonObjOnlyFormatterTable;
+
+#endif
