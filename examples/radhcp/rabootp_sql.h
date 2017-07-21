2c66b71f55495a42c8bacb6ffc86cd68462026e8
Eric Kinzie <eric@qosient.com>
2017-07-21 10:00:45 -0400

    radhcp: add rabootp_sql.[ch]
    These files have mysql support specific to radhcp and are used while
    building database tables of lease information.

---
+++ examples/radhcp/rabootp_sql.h
@@ -0,0 +1,21 @@
+#ifndef __RABOOTP_SQL_H
+# define __RABOOTP_SQL_H
+
+# ifdef HAVE_CONFIG_H
+#  include "argus_config.h"
+# endif
+# include "argus_util.h"
+# include "argus_parser.h"
+# include "argus_print.h"
+# include "rabootp_interval_tree.h"
+
+# if defined(ARGUS_MYSQL)
+int
+RabootpSQLCreateTable(const struct ArgusParserStruct * const parser,
+                      const char * const table);
+int
+RabootpSQLInsertOne(const struct ArgusParserStruct * const parser,
+                    const struct ArgusDhcpIntvlNode *node,
+                    const char * const table);
+# endif
+#endif
