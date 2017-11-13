78503ee9ffc5012382e306c2777f6b7e8695893a
Carter Bullard <carter@qosient.com>
2017-11-13 09:32:39 -0500

    Add daily perl scripts for finding scanners.
    Introduce rascanner.pl to clients, and modify README and Makefile.in.

---
+++ examples/rascanner/rascanner.pl
@@ -0,0 +1,181 @@
+#!/usr/bin/perl
+# 
+#   Gargoyle Client Software.  Tools to read, analyze and manage Argus data.
+#   Copyright (c) 2000-2017 QoSient, LLC
+#   All Rights Reserved
+#  
+#  THE ACCOMPANYING PROGRAM IS PROPRIETARY SOFTWARE OF QoSIENT, LLC,
+#  AND CANNOT BE USED, DISTRIBUTED, COPIED OR MODIFIED WITHOUT
+#  EXPRESS PERMISSION OF QoSIENT, LLC.
+#  
+#  QOSIENT, LLC DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+#  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+#  AND FITNESS, IN NO EVENT SHALL QOSIENT, LLC BE LIABLE FOR ANY
+#  SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+#  IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+#  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+#  THIS SOFTWARE.
+# 
+#   scanner - 
+#
+#
+# % scanner.pl -time -1d -thresh 64
+#
+#
+
+# Complain about undeclared variables
+use v5.6.0;
+use strict;
+use warnings;
+
+$ENV{'PATH'} = '/bin:/usr/bin:/usr/local/bin:/opt/local/lib/mariadb/bin';
+
+# Used modules
+use POSIX;
+use CGI qw(:standard);
+
+use File::Temp qw/ tempfile tempdir /;
+use Time::Local;
+
+my $query = new CGI;
+
+# Global variables
+my $VERSION = "5.0.3";
+my @arglist = ();
+
+my $tdate        = `date`;
+my $remote       = $query->remote_host();
+my $soname       = $query->server_name();
+my $port         = $query->server_port();
+
+chomp $tdate;
+
+my $time         = $query->param('tm');
+my $thresh       = $query->param('te');
+my $database     = $query->param('db');
+my $filter       = $query->param('fi');
+my $cidr         = "24";
+my $elements     = "";
+
+my $debug        = 0;
+my $done         = 0;
+my $mode         = "local";
+
+my $f;
+my $fname;
+
+($f,  $fname)    = tempfile();
+
+# Parse the arguements if any
+
+ARG: while (my $arg = shift(@ARGV)) {
+    if (!$done) {
+      for ($arg) {
+         s/^-debug$//      && do { $debug++; next ARG; };
+         s/^-t$//          && do { $time = shift(@ARGV); next ARG; };
+         s/^-time$//       && do { $time = shift(@ARGV); next ARG; };
+         s/^-dbase$//      && do { $database = shift(@ARGV); next ARG; };
+         s/^-thresh$//     && do { $thresh = shift(@ARGV); next ARG; };
+         s/^-cidr$//       && do { $cidr = shift(@ARGV); next ARG; };
+
+         s/^-local$//      && do { $mode = "local"; $filter = "src loc gte 3 and dst loc gte 3"; next ARG; };
+         s/^-remote$//     && do { $mode = "remote"; $filter = "src loc lt 3 and dst loc lt 3"; next ARG; };
+         s/^-outsidein$//  && do { $mode = "outside"; $filter = "src loc lt 3 and dst loc gte 3"; next ARG; };
+         s/^-insideout$//  && do { $mode = "inside"; $filter = "src loc gte 3 and dst loc lt 3"; next ARG; };
+
+      }
+    } else {
+      for ($arg) {
+         s/\(/\\\(/        && do { ; };
+         s/\)/\\\)/        && do { ; };
+      }
+    }
+    $arglist[@arglist + 0] = $arg;
+}
+
+my $dburl  = "mysql://root\@localhost/scanners/".$mode."_%Y_%m_%d -M time 1d";
+
+  print "DEBUG: arglist is: '@arglist'\n" if $debug;
+
+  if ($time eq "") {
+     $time = RaTodaysDate();
+  }
+
+  my $hosttable = "";
+  my $tabletime = $time;
+  $tabletime =~ s/\//_/g;
+
+  if (not defined $filter) {
+     $mode = "outside";
+     $filter = "src loc lt 3 and dst loc gte 3";
+  }
+  if (not defined $thresh) {
+     $thresh = 128;
+  }
+  $filter = $filter." and trans gte ".$thresh;
+  if ($hosttable eq "") {
+     $hosttable = "host_$tabletime";
+  }
+
+
+  my $cmd = "";
+
+  RaStatusProcessParameters();
+  RaStatusFetchData($fname);
+  RaStatusCleanUp($fname);
+
+sub RaStatusProcessParameters {
+   if (! $database) {
+      $database = "hostsInventory";
+   }
+   $cmd = "mysql -u root $database -NBe \"select addr,count from $hosttable where count > $thresh order by count desc;\"";
+   return;
+}
+
+sub RaStatusFetchData {
+   my $file = shift;
+   my ($i, $k, $v, $data);
+   chomp($file);
+   my $ips = $file.".ips";
+   my $qcmd = "";
+
+   $cmd  = "$cmd > $ips";
+
+   print "DEBUG: RaStatusFetchData: cmd: $cmd\n" if $debug;
+   system($cmd);
+
+   chmod 0644, $ips;
+
+   if (scalar(@arglist) > 0) {
+      $qcmd = "rasql -r mysql://root\@localhost/ipMatrix/ip_$tabletime -w - @arglist | rafilteraddr -f $ips -m saddr -w - | racluster -m saddr daddr -w - | ralabel -f /usr/argus/ralabel.local.conf -w - | racluster -m saddr daddr/$cidr -M dsrs='-agr' -w - | rasort -w $file -m saddr trans daddr - $filter; rasqlinsert -r $file  -w $dburl -t $time -m saddr daddr/$cidr -M drop -s stime dur saddr daddr spkts dpkts trans -p3";
+
+      print "DEBUG: RaStatusFetchData: query: $qcmd\n" if $debug;
+      system($qcmd);
+   }
+
+   return;
+}
+
+sub RaStatusCleanUp {
+   my $file = shift;
+   my $ips = $file.".ips";
+   print "DEBUG: deleting '$file $ips'\n" if $debug;
+   unlink($file);
+   unlink($ips);
+   exit 0;
+}
+
+sub RaStatusError {
+   my $msg = shift;
+   my $file = shift;
+   my $ips = $file.".ips";
+   unlink($file);
+   unlink($ips);
+   exit 1;
+}
+
+sub RaTodaysDate {
+  my($day, $month, $year)=(localtime)[3,4,5];
+  return sprintf( "%04d/%02d/%02d", $year+1900, $month+1, $day);
+}
