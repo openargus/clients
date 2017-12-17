471aba71dd2d4ec5d552383b3aaa0ed3a292cb35
Eric Kinzie <eric@qosient.com>
2017-12-17 17:10:09 -0500

    rampc: fix comipler warning about uninitialized variable
    Zero the timestamp of the last curses update; the first update will
    happen immediately.

--- examples/rampc/racurses.c
+++ examples/rampc/racurses.c
@@ -184,7 +184,7 @@ void *
 ArgusCursesProcess (void *arg)
 {
    int done = 0, cnt = 0;
-   struct timeval ntvbuf, *ntvp = &ntvbuf;
+   struct timeval ntvbuf = {0, }, *ntvp = &ntvbuf;
 
    ArgusCursesProcessInit();
 
