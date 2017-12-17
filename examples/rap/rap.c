c1e3def5880f80200949adb10e676c46330b8055
Eric Kinzie <eric@qosient.com>
2017-12-17 17:01:13 -0500

    rap: fix compiler warning for type punned deref
    Null terminate string without casting to an integer.

--- examples/rap/rap.c
+++ examples/rap/rap.c
@@ -1007,7 +1007,7 @@ RaSendArgusRecord(struct ArgusRecordStruct *argus)
                   ArgusParser->Lflag = 0;
             }
 
-            *(int *)&buf = 0;
+            buf[0] = 0;
             ArgusPrintRecord(ArgusParser, buf, argus, MAXSTRLEN);
 
             if (fprintf (stdout, "%s", buf) < 0)
