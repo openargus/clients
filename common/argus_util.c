99f319c58cfc2be17291319573b0f3a5f47e2799
Eric Kinzie <eric@qosient.com>
2018-01-09 14:22:02 -0500

    rename RaSortFileList to reflect what it does
    This function compares two ArgusInput structures.  Can also be static.

--- common/argus_util.c
+++ common/argus_util.c
@@ -394,7 +394,7 @@ void relts_print(char *, uint32_t);
 
 #include <dirent.h>
 
-int RaSortFileList (const void *, const void *);
+static int CompareArgusInput (const void *, const void *);
 static void ArgusSortFileList (struct ArgusInput **, struct ArgusInput **);
 
 #if !defined(HAVE_TIMEGM)
@@ -476,8 +476,8 @@ RaDescend(char *name)
 
 
 
-int
-RaSortFileList (const void *item1, const void *item2)
+static int
+CompareArgusInput (const void *item1, const void *item2)
 {
    struct ArgusInput *input1 = *(struct ArgusInput **) item1;
    struct ArgusInput *input2 = *(struct ArgusInput **) item2;
@@ -508,7 +508,7 @@ ArgusSortFileList (struct ArgusInput **head,
           input = (struct ArgusInput *)input->qhdr.nxt;
       }
 
-      qsort (array, count, sizeof(input), RaSortFileList);
+      qsort (array, count, sizeof(input), CompareArgusInput);
 
       for (i = 0; i < count; i++) {
          ((struct ArgusInput *)array[i])->qhdr.nxt = NULL;
