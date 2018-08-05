02d5a142f16a04a6c071751e614d4e37c20268db
Carter Bullard <carter@qosient.com>
2018-08-05 17:39:13 -0400

    Add LLC support for 'etype' aggregtion object.
    llc ether_type is the packet length ... make it zero for comparisons,
    if less than 1500.

--- common/argus_client.c
+++ common/argus_client.c
@@ -5886,10 +5886,18 @@ ArgusGenerateHashStruct (struct ArgusAggregatorStruct *na,  struct ArgusRecordSt
 
                                  } else {
                                     bcopy (&((char *) p)[offset], ptr, slen);
+
+                                    if (ARGUS_MASK_ETYPE == i) {
+                                       u_short etype = *(u_short *)ptr;
+                                       if (etype < 1500) {
+                                          *(u_short *)ptr = 0;
+                                       }
+                                    }
                                  }
                               }
                               break;
                            }
+
 
                            case ARGUS_MASK_INODE: {
                               if (na->ArgusMaskDefs[i].len > 0) {
@@ -9013,6 +9021,9 @@ ArgusMergeRecords (struct ArgusAggregatorStruct *na, struct ArgusRecordStruct *n
                           
                                     if (bcmp(&e1->ether_dhost, &e2->ether_dhost, sizeof(e1->ether_dhost)))
                                        bzero ((char *)&e1->ether_dhost, sizeof(e1->ether_dhost));
+
+                                    if (e1->ether_type != e2->ether_type) 
+                                       e1->ether_type = 0;
                                     break;
                                  }
                               }
