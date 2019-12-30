695b057a96c6ac663c60c7e1ef99d3f8c51a65ab
Carter Bullard <carter@qosient.com>
2019-12-30 16:07:33 -0500

    Take rasql_udf_cidr out of the distro.

--- examples/rasql_udf_cidr/installdb.sql
+++
@@ -1,9 +0,0 @@
-use mysql;
-
-drop function if exists rasql_udf_cidr_info;
-drop function if exists str_numtowords;
-drop function if exists rasql_compareCidrtoAddr;
-
-create function rasql_udf_cidr_info returns string soname 'rasql_udf_cidr.so';
-create function str_numtowords returns string soname 'rasql_udf_cidr.so';
-create function rasql_compareCidrtoAddr returns integer soname 'rasql_udf_cidr.so';
--- examples/rasql_udf_cidr/uninstalldb.sql
+++
@@ -1,4 +0,0 @@
-use mysql;
-drop function if exists rasql_udf_cidr_info;
-drop function if exists str_numtowords;
-drop function if exists rasql_compareCidrtoAddr;
