772ac56b435217e9cab7327a82d67c31a53d4eab
Carter Bullard <carter@qosient.com>
2021-03-14 12:58:36 -0400

    Fix issues with configure and pythonlib make depend ... remove rapython example

--- examples/rapython/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('argusWgan',
-                    sources = ['argusWgan.c'])
-
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
