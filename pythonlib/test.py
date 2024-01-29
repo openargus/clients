3c51a132ccf54ab9fc001e4c6f1c0d97c1f09fe8
Carter Bullard <carter@qosient.com>
2024-01-29 15:20:59 -0500

    skinny down initial release ...

--- pythonlib/test.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function, division
-
-import argusWgan as aw
-import numpy as np
-
-y_true = np.random.random(size=(2, 3))
-y_pred = np.random.random(size=(2, 3))
-s = aw.argus_critic(y_true, y_pred)
-print(s)
--- unittests/test_argus_timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TEST_ARGUS_TIMER_H
-# define __TEST_ARGUS_TIMER_H
-# include "argus_timer.h"
-
-typedef int (*sleepfunc)(struct argus_timer_wheel *);
-int runtests(unsigned, struct timespec *, struct timespec *,
-             gettimefunc, sleepfunc);
-
-#endif
